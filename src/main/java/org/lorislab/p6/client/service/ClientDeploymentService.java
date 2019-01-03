4135eb5a7de98e374ecbf135c1b40e42ef9cedb5
Andrej Petras <andrej@lorislab.org>
2019-01-04 00:36:51 +0100

    Remove the jshell script dependency.

--- src/main/java/org/lorislab/p6/client/service/ClientDeploymentService.java
+++ src/main/java/org/lorislab/p6/client/service/ClientDeploymentService.java
@@ -15,10 +15,6 @@
  */
 package org.lorislab.p6.client.service;
 
-import jdk.jshell.JShell;
-import jdk.jshell.Snippet;
-import jdk.jshell.SnippetEvent;
-import jdk.jshell.SourceCodeAnalysis;
 import lombok.extern.slf4j.Slf4j;
 import org.lorislab.p6.config.ConfigService;
 
@@ -35,7 +31,6 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.Objects;
 import java.util.Properties;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -59,7 +54,6 @@ public class ClientDeploymentService {
     public void deployment() {
         try {
             log.info("Start client deployment for the {} / {}", application, module);
-            test();
             Properties properties = new Properties();
             try (InputStream in = this.getClass().getResourceAsStream(ConfigService.DEPLOYMENT_DESCRIPTOR)) {
                 if (in != null) {
@@ -105,55 +99,4 @@ public class ClientDeploymentService {
         return null;
     }
 
-    private void test() {
-        log.info("START TEST");
-        try(JShell jshell = JShell.create()){
-            jshell.onSnippetEvent(snippetEvent -> snippetEventHandler(snippetEvent));
-
-            String s = "System.out.println(\"#########################SCRIPT\")";
-            while (true) {
-                // Read source line by line till semicolon (;)
-                SourceCodeAnalysis.CompletionInfo an = jshell.sourceCodeAnalysis().analyzeCompletion(s);
-                if (!an.completeness().isComplete()) {
-                    break;
-                }
-                // If there are any method declaration or class declaration in new lines, resolve it
-                // otherwise execution errors will be thrown
-                jshell.eval(trimNewlines(an.source()));
-                // Exit if there are no more expressions to evaluate. EOF
-                if (an.remaining().isEmpty()) {
-                    break;
-                }
-                // If there is semicolon, execute next seq
-                s = an.remaining();
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    public void snippetEventHandler(SnippetEvent snippetEvent){
-        String value = snippetEvent.value();
-        if(!Objects.isNull(value) && value.trim().length() > 0) {
-            // Prints output of code evaluation
-            System.out.println(value);
-        }
-
-        // If there are any erros print and exit
-        if(Snippet.Status.REJECTED.equals(snippetEvent.status())){
-            System.out.println("Evaluation failed : "+snippetEvent.snippet().toString()+"\nIgnoring execution of above script");
-        }
-    }
-
-    private String trimNewlines(String s) {
-        int b = 0;
-        while (b < s.length() && s.charAt(b) == '\n') {
-            ++b;
-        }
-        int e = s.length() -1;
-        while (e >= 0 && s.charAt(e) == '\n') {
-            --e;
-        }
-        return s.substring(b, e + 1);
-    }
 }
